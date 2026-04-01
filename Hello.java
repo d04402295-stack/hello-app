64e641c1baa6418a7ed533858290318b3e57668a
d04402295-stack <d04402295@gmail.com>
2026-04-02 01:18:43 +0530

    UC7: String.join for multiple names

--- Hello.java
+++ Hello.java
@@ -1,9 +1,13 @@
+import java.util.Arrays;
+
 public class Hello {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
         } else {
-            System.out.println("Hello");
+            String result = String.join(", ", args);
+            System.out.println("Hello " + result);
         }
     }
 }
